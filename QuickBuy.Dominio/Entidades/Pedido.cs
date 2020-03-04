7e9b3669315837ff36b6a44d90959b5b2bb5537a
Matheus Pimenta <matheus.pimenta.reis@everis.com>
2020-03-04 17:45:40 -0300

    Criação entidades

---
+++ QuickBuy.Dominio/Entidades/Pedido.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+        public DateTime DataPedido { get; set; }
+        public DateTime DataPrevisaoEntraga { get; set; }
+        public int UsuarioId { get; set; }
+
+        public string CEP { get; set; }
+        public string Estado { get; set; }
+        public string Cidade { get; set; }
+        public string Endereco { get; set; }
+        public string NumeroEndereco { get; set; }
+        public int FormaPagementoId { get; set; }
+        public FormaPagamento FormaPagamento { get; set; }
+        public ICollection<ItemPedido> ItensPedido { get; set; }
+
+    }
+}
---
+++ QuickBuy.Dominio/Entidades/ValueObjects/FormaPagamento.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    public class FormaPagamento
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+
+        public bool EhBoleto
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.Boleto; }
+        }
+        public bool EhCartao
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.CartaoCredito; }
+        }
+        public bool EhDeposito
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.Deposito; }
+        }
+        public bool NaoDefinido
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.NaoDefinido; }
+        }
+    }
+}
